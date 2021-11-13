2f36dc16669b6ab1f18b1c25b5d5b14da0934c36
JimmyHerrera <jimmy_hv1@hotmail.com>
2021-11-13 18:16:19 -0500

    Agregando pruebas unitarias

---
+++ PruebasUnitarias/TestControladorMesa/ControllerTestMesa.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
+using Reservas.Controllers;
+using Reservas.Interfaces;
+using Reservas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebasUnitarias.TestControladorMesa
+{
+    public class ControllerTestMesa
+    {
+        private Mock<IMesa> authRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            authRepository = new Mock<IMesa>();
+
+        }
+        [Test]
+        public void TestIndexListaMesasIsOkCase01()
+        {
+
+            authRepository.Setup(a => a.getLista()).Returns(new List<Mesa>());
+
+            var controller = new MesasController(authRepository.Object);
+
+            var view = controller.Index() as ViewResult;
+
+            Assert.IsInstanceOf<List<Mesa>>(view.Model);
+
+        }
+
+        [Test]
+        public void TestCreateMesaIsOkCase02()
+        {
+            authRepository.Setup(a => a.createMesa(new Mesa()));
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+        
+            tempData["mensaje"] = "La reserva se ha creado correctamente";
+
+            var controller = new MesasController(authRepository.Object)
+            {
+                TempData = tempData
+            };
+
+            var view = controller.Create(new Mesa());
+
+            Assert.IsInstanceOf<RedirectToActionResult>(view);
+        }
+    }
+}
---
+++ PruebasUnitarias/TestControladorReserva/controllerTestReserva.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using NUnit.Framework;
+using Reservas.Controllers;
+using Reservas.Interfaces;
+using Reservas.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebasUnitarias.TestControladorReserva
+{
+    public class controllerTestReserva
+    {
+        private Mock<IReserva> reservaRepository;
+        private Mock<IMesa> mesaRepository;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            reservaRepository = new Mock<IReserva>();
+            mesaRepository = new Mock<IMesa>();
+        }
+
+        [Test]
+        public void TestIndexListaReservasIsOkCase01()
+        {
+
+            reservaRepository.Setup(a => a.getLista("")).Returns(new List<Reserva>());
+
+            var controller = new ReservaController(reservaRepository.Object, null);
+
+            var view = controller.Index("") as ViewResult;
+
+            Assert.IsInstanceOf<List<Reserva>>(view.Model);
+
+        }
+
+        [Test]
+        public void TestCreateReservaIsOkCase02()
+        {
+            reservaRepository.Setup(a => a.createReserva(new Reserva()));
+
+            var httpContext = new DefaultHttpContext();
+            var tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+
+            tempData["mensaje"] = "La reserva se ha creado correctamente";
+
+            var controller = new ReservaController(reservaRepository.Object,null)
+            {
+                TempData = tempData
+            };
+
+            var view = controller.Create(new Reserva());
+
+            Assert.IsInstanceOf<RedirectToActionResult>(view);
+        }
+
+    }
+}
